303d68ea0b0f76bb193b568fda832288a487b8e2
Joshi Minh <43663567+JoshiMinh@users.noreply.github.com>
2025-09-01 17:25:30 +0700

    Refactor root pages into views and controllers

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('account');
+    }
 }
--- app/Http/Controllers/ForgotController.php
+++ app/Http/Controllers/ForgotController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class ForgotController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('forgot');
+    }
 }
